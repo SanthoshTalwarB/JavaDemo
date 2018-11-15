875438bbf7a959a0d8a25db4e891a1712c71e31d
bsanthos <bsanthos@FUCCXJJVHG.asiapacific.hpqcorp.net>
2018-11-15 13:15:34 +0530

    Added Demo.java

---
+++ JavaDemo/src/com/hpe/sm/Demo.java
@@ -0,0 +1,11 @@
+package com.hpe.sm;
+
+public class Demo {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Begin of main method");
+		System.out.println("End of main method");
+	}
+
+}
