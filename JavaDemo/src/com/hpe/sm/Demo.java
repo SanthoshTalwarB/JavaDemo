2012b92b8a6e2d9ac2d04a185c539e6681848984
SanthoshTalwarB <44578614+SanthoshTalwarB@users.noreply.github.com>
2018-11-15 13:17:20 +0530

    Added one more print

--- JavaDemo/src/com/hpe/sm/Demo.java
+++ JavaDemo/src/com/hpe/sm/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) 
 	{
 		System.out.println("Begin of main method");
+		System.out.println("Inside of main method");
 		System.out.println("End of main method");
 	}
 
